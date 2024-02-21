39e3a3e963a141629a2f9d719cc44b46764643a9
Caio Henrique <caiohenriqueviana01@gmail.com>
2024-02-21 13:45:26 -0300

    adicionando class mãe e filhas (herança)

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,5 @@
 import henrique.caio.screenAz.modelos.Filme;
+import henrique.caio.screenAz.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -7,14 +8,21 @@ public class Principal {
         meuFilme.setAnoDeLancamento(1970);
         meuFilme.setDuracaoEmMinutos(180);
 
+
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
         System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
-        // meuFilme.somaDasAvaliaçoes = 10;
-        // meuFilme.totaDeAvaliaçoes = 1;
         System.out.println(meuFilme.pegaMedia());
 
+        Serie lost = new Serie();
+        lost.setNome("lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
     }
 }
