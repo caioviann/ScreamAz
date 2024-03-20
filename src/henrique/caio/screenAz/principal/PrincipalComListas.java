9942062bd85aa459bdec52aa083d3f05648cd2ac
Caio Henrique <caiohenriqueviana01@gmail.com>
2024-03-20 16:33:45 -0300

    a

--- src/henrique/caio/screenAz/principal/PrincipalComListas.java
+++ src/henrique/caio/screenAz/principal/PrincipalComListas.java
@@ -9,8 +9,11 @@ import java.util.ArrayList;
 public class PrincipalComListas {
     public static void main(String[] args) {
         Filme meuFilme = new Filme("O poderoso chefão",1970);
+        meuFilme.avalia(9);
         Filme outroFilme = new Filme("Avatar",2023);
+        meuFilme.avalia(6);
         var filmeDoPaulo = new Filme("Dogville",1970);
+        meuFilme.avalia(10);
         Serie lost = new Serie("lost", 2000);
 
         ArrayList<Tituulo> lista = new ArrayList<>();
@@ -20,7 +23,10 @@ public class PrincipalComListas {
         lista.add(outroFilme);
         lista.add(lost);
         for (Tituulo item: lista) {
-            System.out.println(item);
+            System.out.println(item.getNome());
+            Filme filme = (Filme) item;
+            System.out.println("Classsificação: " + filme.getClassificacao());
+
         }
 
     }
