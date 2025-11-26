921bce1e7b78772c8dfe79deb4512b8389751a2b
Caio Henrique <caiohenriqueviana01@gmail.com>
2025-11-26 20:08:31 -0300

    consumindo api

--- src/henrique/caio/screenAz/principal/PrincipalComBusca.java
+++ src/henrique/caio/screenAz/principal/PrincipalComBusca.java
@@ -1,5 +1,8 @@
 package henrique.caio.screenAz.principal;
 
+import com.google.gson.Gson;
+import henrique.caio.screenAz.modelos.Tituulo;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -9,20 +12,27 @@ import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
-        Scanner keyboard = new Scanner(System.in);
-        System.out.print("Digite um filme: ");
-        var busca = keyboard.nextLine();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um Filme para busca: ");
+        var search = sc.nextLine();
 
-        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=ce3ed0d3";
+        String address = "http://www.omdbapi.com/?t=" + search + "&apikey=ce3ed0d3";
 
+
+        //mais informações no JAVA DOC
         HttpClient client = HttpClient.newHttpClient();
-
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
+                .uri(URI.create(address))
                 .build();
+
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(response.body());
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new Gson();
+        Tituulo meuTitulo = gson.fromJson(json, Tituulo.class);
+        System.out.println(meuTitulo.getNome());
     }
 }
